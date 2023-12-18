72cdbc1490f1220b7d0bfb200d848fa9bad84efe
ronitchawla <ronit.chawla2008@gmail.com>
2023-12-18 19:30:36 +0530

    added delete functionality

--- models/Audio.js
+++ models/Audio.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const audioSchema = new mongoose.Schema({
   title    : String,
   audio    : String,
+  cName    : String,
   category : {
     type : mongoose.Types.ObjectId,
     ref  : 'Category',
--- routes/post.js
+++ routes/post.js
@@ -14,7 +14,7 @@ router.post(
     // 3. save audio
     // 4. redirect back
     const { title, category } = req.body;
-    console.log(title, category, req.file);
+    // console.log(title, category, req.file);
     try {
       const cat = await Category.findOne({
         title : category.toLowerCase(),
@@ -26,15 +26,18 @@ router.post(
         api_key    : process.env.API_KEY,
         api_secret : process.env.API_SECRET,
       });
-      const {
-        secure_url,
-      } = await cloudinary.uploader.upload(req.file.path, {
-        resource_type : 'auto',
-      });
+      const a = await cloudinary.uploader.upload(
+        req.file.path,
+        {
+          resource_type : 'auto',
+        }
+      );
+      // console.log(a);
       const audio = new Audio({
         title,
         category : cat._id,
-        audio    : secure_url,
+        audio    : a.secure_url,
+        cName    : a.public_id,
       });
       await audio.save();
       console.log(audio);
@@ -67,6 +70,25 @@ router.post('/category', async (req, res, next) => {
   } catch (err) {}
   console.log(category);
   res.redirect('back');
+});
+
+router.post('/:id/delete', async (req, res, next) => {
+  // 1. get id
+  // 2. find audio with and extract cName
+  // 3. delete from cloudinary and from mongodb
+  // 4. return 200 {'status:'success'}
+  const { id } = req.params;
+  try {
+    const audio = await Audio.findById(id);
+    cloudinary.config({
+      cloud_name : process.env.CLOUD_NAME,
+      api_key    : process.env.API_KEY,
+      api_secret : process.env.API_SECRET,
+    });
+    await cloudinary.uploader.destroy(audio.cName);
+    await Audio.findByIdAndDelete(id);
+  } catch (err) {}
+  return res.status(200).json({ status: 'success' });
 });
 
 module.exports = router;
